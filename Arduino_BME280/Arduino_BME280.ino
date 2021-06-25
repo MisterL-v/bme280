7d61f9f7f580f531635c496ee80deffd82023a75
MisterL-v <leonardvoss0@gmail.com>
2021-06-25 16:41:33 +0200

    Configuration Update
    Now it's possibile to update the configuration of your devices easy. Also their is a support for Wifi-credentials and our new developer mode too.

--- Arduino_BME280/Arduino_BME280.ino
+++ Arduino_BME280/Arduino_BME280.ino
@@ -15,18 +15,32 @@
 */
 
 // Librarys
+#include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <ArduinoJson.h>
 
 // Device-Specific
-int BAUD_RATE = 115200;
-int deep_sleep = 1800; //Sendefrequenz in Sekunden
-long d_id = 0;
-String d_pin = ""; 
-String a_token = "";
-String device_hostname = "";
+long d_id;
+String d_pin;
+String a_token;
+String d_hostname;
+boolean d_dev_mode;
+String d_status;
+long d_baudrate;
+long d_transmission;
+
+// Board Functions
+#define CONFIGURATION_MODE true
+#define DEVELOPER_MODE true
+#define RESTART_CONFIGURATION false
+#define BUZZER D8
+#define RED_LED D5
+#define GREEN_LED D4
+#define BLUE_LED D3
+#define DELAY_ALERT 10000
 
 // BME-280-Sensor
 Adafruit_BME280 bme;
@@ -35,95 +49,96 @@ float w_pressure;
 float w_humidity;
 
 // WiFi-Access
-#define STASSID ""
-#define STAPSK  ""
-const char* ssid     = STASSID;
-const char* password = STAPSK;
+#define WIFI_TIMEOUT 300000
+String ssid;     
+String password;
 
 // Network
 const char* host = "www.datakrash.net"; // example: raspberrypi
 const uint16_t httpPort = 80;
 String path = "/api/weatherdata/addsensordata.php"; // example: /api/data/addsensor.php
 String path_reports = "/api/reports/addreport.php";
+
 WiFiClient client;
 
 // Start of the Program
 void setup() {
-  Serial.begin(BAUD_RATE);
-  delay(2500);
-  Serial.println("Booting...");
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+  set_rgb_led("off");
+  load_settings();
+  Serial.begin(d_baudrate);
   delay(5000);
-  Serial.println("----> Setup <----");
+  Serial.println("Booting and Initializing Datakrash-Board.");
+  post_device_informations();
+  delay(5000);
   connect_to_network();
+  configuration_mode();
   initialize_sensor();
-  Serial.println("Setup Done.");
+  Serial.println("Completed Board-Setup.");
 }
 
 // Program Loop
 void loop() { 
-  Serial.println("----> Loop <----");
   printValues();
-  storedata();
+  store_sensor_data();
   deepsleep();
 }
 
 // Initialize of an network connection
 void connect_to_network() {
-  Serial.println("-- Network Connection --");
+  Serial.println("[Network Connection]");
   Serial.print("Connecting to ");
   Serial.print(ssid);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) { // Verbindungsaufbau
+    set_rgb_led("green");
+    delay(500);
+    set_rgb_led("off");
+    Serial.print(".");
     delay(500);
     Serial.print(".");
+    int connection_time = millis();
+    if (connection_time >= WIFI_TIMEOUT) {
+      set_rgb_led("red");
+      Serial.println("WiFi Timeout detected -> Automatic Restart");
+      delay(DELAY_ALERT);
+      ESP.restart();
+    }
   }
+  set_rgb_led("green");
   Serial.println("");
-  WiFi.hostname(device_hostname);
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
+  Serial.println("WiFi Connected");
+  WiFi.hostname(d_hostname);
+  Serial.println("Device Hostname: " + String(d_hostname));
+  Serial.print("IP-Address of Device: ");
   Serial.println(WiFi.localIP());
 }
 
 // Initialize of sensor
 void initialize_sensor() {
-  Serial.println("-- BME280 Test --");
+  Serial.println("[BME280 Test]");
   bool status;
   status = bme.begin(0x76);  
   if (!status) { // Initialisierung des Sensors per I²C
+    set_rgb_led("red");
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    String error_wire = "204";
-    String error_wire_details = "Could%20not%20find%20a%20valid%20BME280%20sensor,%20check%20wiring!";
-    Serial.println("-- Saving Values in DB --");
-    client.connect(host, httpPort);
-    delay(10000);
-    if (!client.connect(host, httpPort)) {
-      Serial.println("connection failed");
-      return;
-    }
-    String url_reports = String(path_reports) + "?" + "d_id=" + String(d_id) + "&d_pin=" + String(d_pin) + "&a_token=" + String(a_token) + "&r_errorcode=" + String(error_wire) + "&r_details=" + String(error_wire_details);
-    Serial.print("Requesting URL: ");
-    Serial.println(url_reports);
-    client.print(String("GET ") + url_reports + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n"); // Daten werden per GET-Befehl an Server übergeben
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-      if (millis() - timeout > 5000){
-        Serial.println(">>> Client Timeout !");
-        client.stop(); return; 
-      } 
-    }
-    while (client.available()) { 
-      String line = client.readStringUntil('\r'); Serial.print(line);
-    }
-    Serial.println();
-    client.stop();
+    delay(DELAY_ALERT);
+    Serial.println("");
+    store_report("204", "Could%20not%20find%20a%20valid%20BME280%20sensor,%20check%20wiring!");
     ESP.restart();
+  }
+  else {
+    set_rgb_led("green"); 
+    Serial.println("Detected BME-280 Sensor successfully"); 
   }
 }
 
 // Serial output of sensor values
 void printValues() {
-  Serial.println("-- Sensor Values --");
+  Serial.println("[Sensor Measurement]");
   
   w_temperature = bme.readTemperature();
   Serial.print("Temperature = ");
@@ -141,39 +156,18 @@ void printValues() {
   Serial.println(" %");
 
   if(w_pressure == 0 or w_pressure == NULL or w_humidity == NULL or w_temperature == NULL) {
-      Serial.println("Invalid sensor measure. [NULL] Automatic restart.");
-      String error_values = "416";
-      String error_values_details = "Invalid%20sensor%20measure%20[NULL].";
-      Serial.println("-- Saving Values in DB --");
-      client.connect(host, httpPort);
-      delay(10000);
-      if (!client.connect(host, httpPort)) {
-        Serial.println("connection failed");
-        return;
-      }
-      String url_reports = String(path_reports) + "?" + "d_id=" + String(d_id) + "&d_pin=" + String(d_pin) + "&a_token=" + String(a_token) + "&r_errorcode=" + String(error_values) + "&r_details=" + String(error_values_details);
-      Serial.print("Requesting URL: ");
-      Serial.println(url_reports);
-      client.print(String("GET ") + url_reports + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n"); // Daten werden per GET-Befehl an Server übergeben
-      unsigned long timeout = millis();
-      while (client.available() == 0) {
-        if (millis() - timeout > 5000){
-          Serial.println(">>> Client Timeout !");
-          client.stop(); return; 
-        } 
-      }
-      while (client.available()) { 
-        String line = client.readStringUntil('\r'); Serial.print(line);
-      }
-      Serial.println();
-      client.stop();
-      ESP.restart();
+    set_rgb_led("red");
+    Serial.println("Invalid sensor measure. [NULL] Automatic restart.");
+    delay(DELAY_ALERT);
+    Serial.println("");
+    store_report("416", "Invalid%20sensor%20measure%20[NULL].");
+    ESP.restart();
   }
 }
 
 // Save and store data in database
-void storedata() {
-  Serial.println("-- Saving Values in DB --");
+void store_sensor_data() {
+  Serial.println("[Saving new sensor data.]");
   client.connect(host, httpPort);
   delay(10000);
   if (!client.connect(host, httpPort)) {
@@ -201,9 +195,255 @@ void storedata() {
 
 // Deepsleep to save energy and ressources
 void deepsleep() {
-  Serial.println("-- Deep Sleep --");
+  Serial.println("[Deep Sleep]");
   Serial.println("Going to deep sleep. Good Night!");
-  Serial.println("Automatic awake in " + String(deep_sleep) + " Seconds");
-  ESP.deepSleep(deep_sleep * 1000000);
+  Serial.println("Automatic awake in " + String(d_transmission) + " Seconds");
+  ESP.deepSleep(d_transmission * 1000000);
   yield();
+}
+
+void set_rgb_led(String color) {
+  if (d_dev_mode == true || DEVELOPER_MODE == true) {
+    if (color == "red") {
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(BLUE_LED, LOW);
+    digitalWrite(RED_LED, HIGH);
+    }
+    if (color == "green") {
+      digitalWrite(RED_LED, LOW);
+      digitalWrite(BLUE_LED, LOW);
+      digitalWrite(GREEN_LED, HIGH);
+    }
+    if (color == "blue") {
+      digitalWrite(GREEN_LED, LOW);
+      digitalWrite(RED_LED, LOW);
+      digitalWrite(BLUE_LED, HIGH);
+    }
+  }
+  if (color == "off") {
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(BLUE_LED, LOW);
+  }
+}
+
+void store_report(String error_values, String error_details) {
+  Serial.println("[Saving a new report]");
+  String url_reports = String(path_reports) + "?" + "d_id=" + String(d_id) + "&d_pin=" + String(d_pin) + "&a_token=" + String(a_token) + "&r_errorcode=" + String(error_values) + "&r_details=" + String(error_details);
+  client.connect(host, httpPort);
+    delay(10000);
+    if (!client.connect(host, httpPort)) {
+      Serial.println("connection failed");
+      return;
+    }
+  Serial.print("Requesting URL: ");
+  Serial.println(url_reports);
+  client.print(String("GET ") + url_reports + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n"); // Daten werden per GET-Befehl an Server übergeben
+  
+  // Check HTTP status
+  char status[32] = {0};
+  client.readBytesUntil('\r', status, sizeof(status));
+  if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
+    Serial.print(F("Unexpected response: "));
+    Serial.println(status);
+    return;
+  }
+
+  // Skip HTTP headers
+  char endOfHeaders[] = "\r\n\r\n";
+  if (!client.find(endOfHeaders)) {
+    Serial.println(F("Invalid response"));
+    return;
+  }
+  return;
+}
+
+void configuration_mode () {
+  if (CONFIGURATION_MODE == true) {
+    Serial.println("[Configuration Mode]");
+    set_rgb_led("blue");
+    client.connect(host, httpPort);
+    delay(10000);
+    if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+    }
+    Serial.print("Requesting URL: ");
+    String path_device_informations = "/api/informations/device_informations.php";
+    String url_device_informations = String(path_device_informations) + "?" + "d_id=" + String(d_id) + "&d_pin=" + String(d_pin) + "&a_token=" + String(a_token);
+    Serial.println(url_device_informations);
+    client.print(String("GET ") + url_device_informations + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n"); // Daten werden per GET-Befehl an Server übergeben
+  
+    // Check HTTP status
+    char status[32] = {0};
+    client.readBytesUntil('\r', status, sizeof(status));
+    if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
+      Serial.print(F("Unexpected response: "));
+      Serial.println(status);
+      return;
+    }
+  
+    // Skip HTTP headers
+    char endOfHeaders[] = "\r\n\r\n";
+    if (!client.find(endOfHeaders)) {
+      Serial.println(F("Invalid response"));
+      set_rgb_led("red");
+      store_report("415","Ungültige%20Antwort%20des%20Servers.");
+      delay(DELAY_ALERT);
+      return;
+    }
+    DynamicJsonDocument doc(768);
+    // Parse JSON object
+    DeserializationError error = deserializeJson(doc, client);
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      set_rgb_led("red");
+      store_report("415","Es%20ist%20ein%20Fehler%20bei%20der%20Deserialisierung%20von%20JSON-Objekten%20aufgetreten.");
+      delay(DELAY_ALERT);
+      return;
+    }
+    bool operation_done = doc["operation_done"]; // true
+    String error_reason = doc["error_reason"]; // ""
+    boolean deserialized_d_dev_mode = doc["d_dev_mode"];
+    d_dev_mode = deserialized_d_dev_mode; 
+    int deserialized_d_id = doc["device_id"]; // "2"
+    d_id = deserialized_d_id;
+    String deserialized_d_hostname = doc["d_name"]; // "Wetterstation-2"
+    d_hostname = deserialized_d_hostname;
+    String deserialized_d_status = doc["d_status"]; // "online"
+    d_status = deserialized_d_status;
+    String deserialized_d_wifi_ssid = doc["d_wifi_ssid"]; // "online"
+    ssid = deserialized_d_wifi_ssid;
+    String deserialized_d_wifi_password = doc["d_wifi_password"]; // "online"
+    password = deserialized_d_wifi_password;
+    long deserialized_d_baudrate = doc["d_baudrate"]; // "115200"
+    d_baudrate = deserialized_d_baudrate;
+    int deserialized_d_transmission = doc["d_transmission"]; // "1800"
+    d_transmission = deserialized_d_transmission;
+    if (operation_done == true) {
+      set_rgb_led("green");
+      update_device_informations();
+      Serial.println("Automatic Device Configuration was successfully.");
+    } else {
+      set_rgb_led("red");
+      Serial.println("Automatic Device Configuration Error.");
+      Serial.print("Reason: ");
+      Serial.println(error_reason);
+      store_report("449","Abfrage%20der%20Geräteinformationen%20ist%20fehlgeschlagen."); 
+    }
+    if (RESTART_CONFIGURATION == true) {
+      Serial.println("Restarting System to release Configuration");
+      delay(DELAY_ALERT);
+      ESP.restart();
+    }
+    delay(DELAY_ALERT);
+  }
+}
+
+void load_settings() {
+  EEPROM.begin(512);
+  d_dev_mode = EEPROM.read(1);
+  d_status = eepromReadString(2);
+  d_id = eepromReadLong(10);
+  d_pin = eepromReadString(20);
+  d_hostname = eepromReadString(40);
+  d_baudrate = eepromReadLong(70);
+  d_transmission = eepromReadLong(90);
+  ssid = eepromReadString(110);
+  password = eepromReadString(150);
+  a_token = eepromReadString(190);
+}
+
+void post_device_informations() {
+  Serial.println("[Loading Device-Informations]");
+  Serial.print("Developer-Mode: ");
+  Serial.println(d_dev_mode);
+  Serial.println("Device-Status: " + d_status);
+  Serial.print("Device-ID: ");
+  Serial.println(d_id);
+  Serial.print("Hostname: ");
+  Serial.println(d_hostname);
+  Serial.print("Baudrate: ");
+  Serial.println(d_baudrate);
+  Serial.print("Transmission: ");
+  Serial.println(d_transmission);
+  Serial.println("SSID: " + ssid);
+}
+
+void update_device_informations() {
+  EEPROM.write(1, d_dev_mode);
+  eepromWriteString(2, d_status);
+  eepromWriteLong(10, d_id);
+  eepromWriteString(20, d_pin);
+  eepromWriteString(40, d_hostname);
+  eepromWriteLong(70, d_baudrate);
+  eepromWriteLong(90, d_transmission);
+  eepromWriteString(110, ssid);
+  eepromWriteString(150, password);
+  eepromWriteString(190, a_token);
+  Serial.println("Updated EEPROM successfully.");
+  post_device_informations();
+}
+
+void eepromWriteString(char add,String data)
+{
+  int _size = data.length();
+  int i;
+  for(i=0;i<_size;i++)
+  {
+    EEPROM.write(add+i,data[i]);
+  }
+  EEPROM.write(add+_size,'\0');   //Add termination null character for String Data
+  EEPROM.commit();
+}
+
+
+String eepromReadString(char add)
+{
+  int i;
+  char data[100]; //Max 100 Bytes
+  int len=0;
+  unsigned char k;
+  k=EEPROM.read(add);
+  while(k != '\0' && len<500)   //Read until null character
+  {    
+    k=EEPROM.read(add+len);
+    data[len]=k;
+    len++;
+  }
+  data[len]='\0';
+  return String(data);
+}
+
+void eepromWriteInt(int adr, int wert) {
+  byte low, high;
+  low=wert&0xFF;
+  high=(wert>>8)&0xFF;
+  EEPROM.write(adr, low); // dauert 3,3ms 
+  EEPROM.write(adr+1, high);
+  return;
+} 
+
+int eepromReadInt(int adr) {
+  byte low, high;
+  low=EEPROM.read(adr);
+  high=EEPROM.read(adr+1);
+  return low + ((high << 8)&0xFF00);
+}
+
+void eepromWriteLong(int address, long number)
+{ 
+  EEPROM.write(address, (number >> 24) & 0xFF);
+  EEPROM.write(address + 1, (number >> 16) & 0xFF);
+  EEPROM.write(address + 2, (number >> 8) & 0xFF);
+  EEPROM.write(address + 3, number & 0xFF);
+}
+
+long eepromReadLong(int address)
+{
+  return ((long)EEPROM.read(address) << 24) +
+         ((long)EEPROM.read(address + 1) << 16) +
+         ((long)EEPROM.read(address + 2) << 8) +
+         (long)EEPROM.read(address + 3);
 }
